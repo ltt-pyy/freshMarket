fe2308fd534d2822c16f8c3ede22f4a567b9efe1
ltltltltltltlt <46098554+ltltltltltltlt@users.noreply.github.com>
2019-05-08 20:55:36 +0800

    Update IEmpDAO.java

--- freshMarket/src/fresh/dao/IEmpDAO.java
+++ freshMarket/src/fresh/dao/IEmpDAO.java
@@ -4,8 +4,8 @@ import java.util.* ;
 
 import fresh.vo.Emp;
 
-public interface IEmployeeDAO {
+public interface IEmpDAO {
 	public boolean doCreate(Employee employee) throws Exception ;
 	public List<Employee> findAll(String keyWord) throws Exception ;
-	public Employee findById(int Employeeno) throws Exception ;
+	public Employee findById(int EmpId) throws Exception ;
 }
