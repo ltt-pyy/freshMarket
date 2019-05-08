aac6dcb1b8d2605cab3b4b99f8cc3ad83ab92aa2
7Monarchess <40885355+7Monarchess@users.noreply.github.com>
2019-05-08 20:04:06 +0800

    配送员信息

---
+++ freshMarket/src/fresh/vo/Emp.java
@@ -0,0 +1,39 @@
+package fresh.vo;
+import java.util.Date ;
+public class Emp {
+	private int empId ;
+	private String empname ;
+	private float wages ;
+	private Date hiredate ;
+	private float sal_sum ;
+	public void setempId(int empId){
+		this.empId = empId ;
+	}
+	public void setempname(String empname){
+		this.empname = empname ;
+	}
+	public void setwages(float wages){
+		this.wages = wages ;
+	}
+	public void setHiredate(Date hiredate){
+		this.hiredate = hiredate ;
+	}
+	public void setsal_sum(float sal_sum){
+		this.sal_sum = sal_sum ;
+	}
+	public int getempId(){
+		return this.empId ;
+	}
+	public String getempname(){
+		return this.empname ;
+	}
+	public float getwages(){
+		return this.wages ;
+	}
+	public Date getHiredate(){
+		return this.hiredate ;
+	}
+	public float getsal_sum(){
+		return this.sal_sum ;
+	}
+}
